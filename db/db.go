7acd01bb23e82d2f3e8e337879e46fb148dc981e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-26 07:05:32 +0500

    db funcs

--- db/db.go
+++ db/db.go
@@ -4,19 +4,56 @@ import (
 	"errors"
 
 	"github.com/alphadev97.com/go-csrf/db/models"
+	"github.com/alphadev97.com/go-csrf/randomstrings"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var users = map[string]models.User{}
+var refreshToken map[string]string
 
-func InitDB()
+func InitDB() {
+	refreshToken = make(map[string]string)
+}
 
 func StoreUser(username string, password string, role string) (uuid string, err error) {
+	uuid, err = randomstrings.GenerateRandomString(32)
+	if err != nil {
+		return "", err
+	}
+
+	u := models.User{}
+	for u != users[uuid] {
+		uuid, err = randomstrings.GenerateRandomString(32)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	PasswordHash, hashErr := generateBcryptHash(password)
+	if hashErr != nil {
+		err = hashErr
+		return
+	}
+
+	users[uuid] = models.User{username, PasswordHash, role}
+	return uuid, err
 
 }
 
-func DeleteUser()
+func DeleteUser(uuid string) (models.User, error) {
+	delete(users, uuid)
+}
 
-func FetchUserById()
+func FetchUserById(uuid string) (models.User, error) {
+	u := users[uuid]
+	blankUser := models.User{}
+
+	if blankUser != u {
+		return u, nil
+	} else {
+		return u, errors.New("user not found that matches the given id")
+	}
+}
 
 func FetchUserByUsername(username string) (models.User, string, error) {
 	for k, v := range users {
@@ -27,7 +64,9 @@ func FetchUserByUsername(username string) (models.User, string, error) {
 	return models.User{}, "", errors.New("User not found that matches the given username")
 }
 
-func StoreRefreshToken()
+func StoreRefreshToken() (jti string, err error) {
+
+}
 
 func DeleteRefreshToken()
 
@@ -35,6 +74,9 @@ func CheckAndRefreshTokens()
 
 func LogUserIn()
 
-func generateBcryptHash()
+func generateBcryptHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash[:]), err
+}
 
 func checkPasswordAgainstHash()
